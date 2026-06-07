9e1301d69c26b7a6414101e1b8f8f743469c86e8
David Luke <daiveedlucas049@gmail.com>
2026-06-06 18:52:37 -0700

    fix: remove oidc_issuer local until eks module is available

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -9,5 +9,4 @@ locals {
   private_subnets = ["10.0.10.0/24", "10.0.11.0/24"]
   azs             = ["${var.region}a", "${var.region}b"]
 
-  oidc_issuer = trimprefix(module.eks.cluster_oidc_issuer_url, "https://")
 }
